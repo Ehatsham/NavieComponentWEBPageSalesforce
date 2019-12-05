87a041f3a911c806230cba679f4e7f2bd31785a9
Ehatsham <ehteshmdanish9@gmail.com>
2019-12-05 18:12:47 +0530

    message

--- force-app/main/default/lwc/relatedContactsInfo/relatedContactsInfo.js
+++ force-app/main/default/lwc/relatedContactsInfo/relatedContactsInfo.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import fetchContacts from '@salesforce/apex/homeOrgClass.fetchContacts';
+export default class relatedContactsInfo extends LightningElement {
+    @api AccountInfo;
+    @track selectId;
+    @track accIdInfo;
+    @track contacts;
+    @track error;
+    //this.selectEvent=false;
 
-export default class relatedContactsInfo extends LightningElement {}
+    contactHandler(event) {
+        this.selectEvent = true;
+        this.accIdInfo = event.AccountId;
+
+    }
+    @wire(fetchContacts, { accId: '$accIdInfo' })
+    wiredData(data, error) {
+        if (data) {
+            this.contacts = data;
+        }
+        if (error) {
+            this.error = error;
+        }
+    }
+
+
+}
